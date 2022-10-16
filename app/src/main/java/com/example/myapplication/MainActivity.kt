7c2b529d6ad20566e0bc49555403c3c583d7146c
muhyasin89 <muhyasin89@gmail.com>
2022-10-16 20:14:13 +0700

    [state_jetpack]change color

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,49 +1,38 @@
 package com.example.myapplication
 
-import android.accounts.AuthenticatorDescription
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            val painter = painterResource(id = R.drawable.kermit_in_the_snow)
-            val description = "Kermit in the snow"
-            val title = "Kermit is playing in snow"
-            Box(
-                modifier = Modifier.fillMaxSize(0.5f).padding(16.dp)
-            ){
-                ImageCard(
-                    painter=painter,
-                    contentDescription = description,
-                    title=title
+        setContent{
+            val col = remember {
+                mutableStateOf(Color.Yellow)
+            }
+            Column(Modifier.fillMaxSize()) {
+                ColorBox(
+                    Modifier.weight(1f).fillMaxSize()
+                ) {
+                    col.value =it
+                }
+                Box(modifier = Modifier
+                    .background(col.value)
+                    .weight(1f)
+                    .fillMaxSize()
                 )
             }
 
@@ -52,42 +41,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier=Modifier
-){
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(20.dp),
-        elevation= 5.dp) {
-        Box(modifier = Modifier.height(250.dp)){
-            Image(painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
-            )
-            Box(modifier = Modifier
-                .fillMaxSize(0.5f)
-                .background(
-                    Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black
-                        ),
-                        startY = 300f
-                    )
+fun ColorBox(modifier: Modifier = Modifier, updateColor: (Color) -> Unit){
+
+
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
                 )
             )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(3.dp),
-                contentAlignment = Alignment.BottomStart
-            ){
-                Text(title, style= TextStyle(color = Color.White, fontSize = 16.sp))
-            }
+
         }
-    }
+    )
 }
+
