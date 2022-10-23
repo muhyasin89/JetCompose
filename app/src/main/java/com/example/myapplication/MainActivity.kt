45ef71b37d4a8d70d3577662e8b1d3f1db36321f
muhyasin89 <muhyasin89@gmail.com>
2022-10-23 17:04:07 +0700

    final side effect

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -17,37 +17,38 @@ import androidx.compose.ui.unit.*
 import androidx.compose.ui.*
 import androidx.compose.ui.text.font.*
 import androidx.compose.ui.text.style.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val scaffoldState = rememberScaffoldState()
+            val scope = rememberCoroutineScope()
+            Scaffold(scaffoldState=scaffoldState) {
+//                var counter by remember{
+//                    mutableStateOf(0)
+//                }
 
-        }
-    }
-}
+                var counter = produceState(initialValue = 0){
+                    delay(3000L)
+                    value = 4
+                }
 
-var i=0
+                if(counter.value % 5 == 0 && counter.value > 0 ){
+                    LaunchedEffect(key1 = scaffoldState.snackbarHostState) {
+                        scaffoldState.snackbarHostState.showSnackbar("Hello")
+                    }
 
-@Composable
-fun myComposable(backPressedDispatcher: OnBackPressedDispatcher){
+                }
 
-    val callback = remember{
-        object: OnBackPressedCallback(true){
-            override fun handleOnBackPressed() {
-                // Do Something
+                Button(onClick = {}){
+                    Text("Click Me: $counter.value}")
+                }
             }
         }
     }
-
-
-    DisposableEffect(key1 = backPressedDispatcher){
-        backPressedDispatcher.addCallback(callback)
-        onDispose{
-            callback.remove()
-        }
-    }
-    Button(onClick = { /*TODO*/ }) {
-        Text(text="Click Me")
-    }
 }
+
+
