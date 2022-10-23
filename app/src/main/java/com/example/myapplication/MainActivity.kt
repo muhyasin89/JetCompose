5dc3d511d3b0902426f8f5c3568a5c4255673182
muhyasin89 <muhyasin89@gmail.com>
2022-10-23 16:45:04 +0700

    Side Effect

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.myapplication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,6 +12,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 
 import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.unit.*
 import androidx.compose.ui.*
 import androidx.compose.ui.text.font.*
@@ -20,22 +23,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
-            LazyColumn{
-                itemsIndexed(
-                    listOf("This", "is", "JetPack", "Compose")
-                ){
-                    index, string->
-                    Text(text = string,
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 24.dp)
-                    )
-                }
+        }
+    }
+}
 
+var i=0
+
+@Composable
+fun myComposable(backPressedDispatcher: OnBackPressedDispatcher){
+
+    val callback = remember{
+        object: OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                // Do Something
             }
         }
+    }
+
+
+    DisposableEffect(key1 = backPressedDispatcher){
+        backPressedDispatcher.addCallback(callback)
+        onDispose{
+            callback.remove()
+        }
+    }
+    Button(onClick = { /*TODO*/ }) {
+        Text(text="Click Me")
     }
 }
